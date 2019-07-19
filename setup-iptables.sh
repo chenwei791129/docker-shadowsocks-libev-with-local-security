c1c3d2e802455563944b4bcaffccfc0dd5dbeef9
AwEi <chenwei791129@livemail.tw>
2019-07-19 21:33:05 +0800

    test

--- setup-iptables.sh
+++ setup-iptables.sh
@@ -11,12 +11,12 @@ iptables -P FORWARD ACCEPT
 #iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Allow gateway ip address
-iptables -A OUTPUT -d $GATEWAY_IP -j ACCEPT
+#iptables -A OUTPUT -d $GATEWAY_IP -j ACCEPT
 
 # DROP Private subnet
-iptables -A OUTPUT -d 10.0.0.0/8 -j DROP
-iptables -A OUTPUT -d 172.16.0.0/12 -j DROP
-iptables -A OUTPUT -d 192.168.0.0/16 -j DROP
+#iptables -A OUTPUT -d 10.0.0.0/8 -j DROP
+#iptables -A OUTPUT -d 172.16.0.0/12 -j DROP
+#iptables -A OUTPUT -d 192.168.0.0/16 -j DROP
 
 # Fix udp error https://github.com/shadowsocks/shadowsocks-libev/issues/1619
 #iptables -A FORWARD -m state --state UNTRACKED -j ACCEPT
